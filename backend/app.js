8cb6be4bc3c8b41ffd5e6c81791a77020b9a75a5
Qisth <qisthgames@gmail.com>
2026-05-19 21:26:22 +0700

    Add cart endpoints

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const cookieParser = require("cookie-parser");
 const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
 const productRoutes = require("./routes/productRoutes");
+const cartRoutes = require("./routes/cartRoutes");
 
 const app = express();
 
@@ -13,5 +14,6 @@ app.use(cookieParser());
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 app.use("/products", productRoutes);
+app.use("/cart", cartRoutes);
 
 module.exports = app;
