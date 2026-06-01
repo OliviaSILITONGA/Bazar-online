3b6cd351e9c3eebf19823f512624d8098e13d67f
cinciau <cindyaulia2327@gmail.com>
2026-06-01 14:44:17 +0700

    commit
    yeayy dah bisa cartnyaa

--- frontend/js/product.js
+++ frontend/js/product.js
@@ -258,7 +258,7 @@ async function addToCart() {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        product_id: productId,
+        product_id: parseInt(productId),
         quantity: qty,
       }),
     });
